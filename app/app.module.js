410b549900da46fa769df025ae66adf2a0305ec8
Subash PS <sp00345364@techmahindra.com>
2017-03-04 11:56:50 +0530

    https issue

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,8 @@
 'use strict';
 var myapp = angular.module("myapp",['ngRoute','ui.grid','ui.grid.selection', 'ui.grid.edit','ui.grid.cellNav','ngResource','ui.bootstrap']);
+myApp.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
 myapp.constant('BASE_URL','https://themonkfc.phptipsntricks.com/index.php/');
